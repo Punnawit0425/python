61693aa47ea9acb46a7e27ca964544797edf5eb9
Punnawit0425 <punnawitarunyanart@gmail.com>
2026-06-02 23:16:37 +0700

    Calculator

--- calculator/calculatorpopup.py
+++ calculator/calculatorpopup.py
@@ -1,35 +1,46 @@
 import tkinter as tk
-root = tk.Tk()
-root.title("Calculator")
-root.resizable(False,False)
+tkin = tk.Tk()
+tkin.geometry("400x500")
 
-display = tk.Entry(root, font=("Arial", 20),justify="right",bd = 5) 
-display.grid(row=0,column = 0, columnspan = 4)
+for i in range(4):
+    tkin.grid_columnconfigure(i, weight=1)
 
-def click(value):
-    display.insert(tk.END, value)
+for i in range(5):
+    tkin.grid_rowconfigure(i, weight=1)
+tkin.title("Calculator")
+
+entry = tk.Entry(tkin, font=("Arial", 24))
+
+entry.grid(row=0,column=0,columnspan=4, sticky="nsew")
+
+def display(number):
+    entry.insert(tk.END,number)
+
 def clear():
-    display.delete(0, tk.END)
+    entry.delete(0,tk.END)
+
 def calculate():
-    try:
-        result = eval(display.get())
-        clear()
-        display.insert(0, result)
-    except:
-        clear()
-        display.insert(0,"Error")
-buttons = [["7","8","9","/"],
-           ["4","5","6","*"],
-           ["1","2","3","-"],
-           ["0",".","=","+"],
-           ]
-for r, row in enumerate(buttons, start=1):
-    for c, label in enumerate(row):
-        cmd = calculate if label == "=" else lambda v=label: click(v)
-        tk.Button(root, text=label, width=5, height=2,
-                  font=("Arial", 14), command=cmd).grid(row=r, column=c)
+    result = eval(entry.get())
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,result)
 
-tk.Button(root, text="C", width=23, height=2,
-          font=("Arial", 14), command=clear).grid(row=5, column=0, columnspan=4)
+    
+buttons = [
+    ["7", "8", "9", "/"],
+    ["4", "5", "6", "*"],
+    ["1", "2", "3", "-"],
+    ["0", "C", "=", "+"]
+]
+for row_num, rows in enumerate(buttons):
+    for col_num, column in enumerate(rows):
+        if column == "C":
+            button = tk.Button(tkin,text="C",font=("Arial", 20),command=clear)
+            button.grid(row=row_num + 1, column=col_num, sticky="nsew")
+        elif column == "=":
+            button = tk.Button(tkin,text="=",font=("Arial", 20),command=calculate)
+            button.grid(row=row_num + 1, column=col_num, sticky="nsew")
+        else:
+            button = tk.Button(tkin,text=column,font=("Arial", 20),command=lambda value=column: display(value))
+            button.grid(row=row_num + 1, column=col_num, sticky="nsew")
 
-root.mainloop()
+tkin.mainloop()
